a92b20ae1e614923f0b60ffd2c7f08278aeda7d7
kamilyte <kamilekaskelyte@gmail.com>
2023-03-31 10:59:54 +0200

    created lead table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './styles/App.css';
 import TopMenuBar from "./components/topMenuBar.js";
 import SideMenuBar from "./components/sideMenuBar";
+import LeadTable from './components/leadTable';
 
 function App() {
     return (
@@ -14,8 +15,8 @@ function App() {
                     <div className="TopMenuBarBox">
                         <TopMenuBar />
                     </div>
-                    <div className="TableContentBox">
-                        table
+                    <div className="TableContent">
+                        <LeadTable />
                     </div>
                 </div>
             </div>
---
+++ frontend/src/components/leadTable.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material"
+
+function createData(profile, name, ranking, industry, linkedin, leadList) {
+    return { profile, name, ranking, industry, linkedin, leadList }
+}
+
+const leads = [
+    createData('Profile', 'Jane', '3 stars', 'Retail', 'Link', 'E-commerce')
+]
+
+const LeadTable = () => {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{minWidth: 650}} aria-label="lead table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Profile</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Ranking</TableCell>
+                        <TableCell>Industry</TableCell>
+                        <TableCell>Linkedin</TableCell>
+                        <TableCell>Lead List</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {leads.map((lead) => (
+                        <TableRow key={lead.profile} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                            <TableCell component="th" scope="row">{lead.profile}</TableCell>
+                            <TableCell>{lead.name}</TableCell>
+                            <TableCell>{lead.ranking}</TableCell>
+                            <TableCell>{lead.industry}</TableCell>
+                            <TableCell>{lead.linkedin}</TableCell>
+                            <TableCell>{lead.leadList}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
+export default LeadTable
