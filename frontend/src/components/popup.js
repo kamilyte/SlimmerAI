d4b87abb62de87e0f4a2ba6263dfa581a1a673d0
kamilyte <kamilekaskelyte@gmail.com>
2023-05-06 16:23:21 +0200

    started styling popup based on concept

--- frontend/src/components/popup.js
+++ frontend/src/components/popup.js
@@ -1,14 +1,47 @@
 import React, {Component} from "react"
 import '../styles/popup.css'
+import CloseIcon from '@mui/icons-material/Close';
+import { IconButton } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { grey } from "@mui/material/colors";
 
 class Popup extends Component {
+    state = {
+        popup: true
+    }
 
+    handleCloseClick = () => {
+        this.setState({popup: false})
+    }
 
 
     render() {
         return (
                 <div className="PopupCard">
-                    popup
+                    <img className="Popup-profile">
+                    </img>
+                    <h2 className="Popup-name">
+                        Name
+                    </h2>
+                    <h2 className="Popup-position">
+                        Position @ Company
+                    </h2>
+                    <div className="Location">
+                        <LocationOnIcon sx={{position: "absolute", color: grey[400], fontSize: "1.5vw", left: "0vw", top: "0.25vw"}}/>
+                        <h2>
+                            Location, Country
+                        </h2>
+                    </div>
+                    <div className="LeadList">
+                        Lead-List
+                    </div>
+                    <IconButton className="Close" size="small">
+                        <CloseIcon fontSize="inherit"/>
+                    </IconButton>
+                    <IconButton className="Edit" size="small">
+                        <EditIcon fontSize="inherit"/>
+                    </IconButton>
                 </div>
         )
     }
