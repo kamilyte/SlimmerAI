f3525add84ed0a784961bef6d9e475ad2efb0609
kamilyte <kamilekaskelyte@gmail.com>
2023-05-18 12:31:11 +0200

    small changes

--- frontend/src/components/topMenuBar.js
+++ frontend/src/components/topMenuBar.js
@@ -3,6 +3,7 @@ import '../styles/topMenuBar.css';
 import dotDropDown from "../resources/dotDropDown.svg";
 import SearchBar from "./searchBarComponent";
 import Filter from "./filter";
+import { Grid } from "@mui/material";
 
 class TopMenuBar extends Component {
     state = {clicked: false};
@@ -16,13 +17,33 @@ class TopMenuBar extends Component {
         return (
             <div className="TopMenuBarBox">
                 <div className={this.state.clicked ? "TopMenuBarExpandedContent" : "TopMenuBar"}>
-                    <SearchBar />
-                    <Filter />
+                   
+                    <Grid container spacing={3}>
+                        <Grid xs={6}>
+                            <SearchBar> </SearchBar>
+                        </Grid>
+                        <Grid xs>
+                        <Filter />
+                        </Grid>
+                        <Grid xs>
+                        <Filter />
+                        </Grid>
+                        <Grid xs>
+                        <Filter />
+                        </Grid>
+                        <Grid xs>
+                        <Filter />
+                        </Grid>
+                        <Grid xs>
+                        <Filter />
+                        </Grid>
+                        </Grid>
                     <span className="TopBarDropDownSelection place-content-center">
                         <button type="button" onClick={this.handleClick} className="TopBarDropDownButton place-content-center">
                             <img className="DropDownIcon" src={dotDropDown} alt="3 Dot Dropdown Icon" />
                         </button>
                     </span>
+
                 </div>
             </div>
         )
